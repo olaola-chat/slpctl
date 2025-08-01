772505bcb2572f1ae41489c58c3b385efcf46aa7
hugh <13147199592@163.com>
2025-08-01 17:51:08 +0800

    22

--- helmgen/helm.go
+++ helmgen/helm.go
@@ -26,7 +26,7 @@ const htmlTemplate = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>服务管理仪表盘</title>
+    <title>helm重启tag服务管理仪表盘</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
